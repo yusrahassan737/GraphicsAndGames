f383e37c3a7705809803eb96246977c46319cec1
Yusra Hassan <77341222+yusrahassan737@users.noreply.github.com>
2021-04-24 22:51:05 -0400

    Update turtle experimentation.py

--- turtle experimentation.py
+++ turtle experimentation.py
@@ -1,12 +1,18 @@
-import turtle
+# Name: Yusra Hassan
+# Date: July 22, 2020
+# Decription: Draws flower made of colourful circles
+# Purpose: Practice with turtle drawing
 
+# Start-up
+import turtle
 colours = ["#91576e", "#58b09d", "#93d65c", "#e0ae82", "#e88079"]
 
+# Loops to draw
 for i in range(5):
     for j in range(5):
         turtle.fillcolor(colours[j])
         turtle.begin_fill()
-        turtle.circle(70)
+        turtle.circle(50)
         turtle.end_fill()
         turtle.right(30)
         
